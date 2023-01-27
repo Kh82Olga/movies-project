3c2dd69bde5de13fb9d991ac31c7e89c784cc2b4
Kh82Olga <kh82olga@protonmail.com>
2023-01-27 15:47:28 +0100

    2701

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,32 +1,49 @@
 import React from "react";
 
 class Search extends React.Component {
-    state={
-        search: '',
-    }
+  state = {
+    search: "",
+    type: 'all'
+  };
 
-    handleKey = (event) => {
-        if(event.key==='Enter'){
-           this.props.searchMovies(this.state.search);
-        }
+  handleKey = (event) => {
+    if (event.key === "Enter") {
+      this.props.searchMovies(this.state.search);
     }
+  };
+  handleFilter = (event)=>{
+    
+  }
 
   render() {
     return (
       <div className="row">
-        
-          <div className="input-field">
-            <input 
-            className="validate" 
-            placeholder="search" 
-            type="search" 
+        <div className="input-field">
+          <input
+            className="validate"
+            placeholder="search"
+            type="search"
             value={this.state.search}
-            onChange={(e) => this.setState({search: e.target.value})}
+            onChange={(e) => this.setState({ search: e.target.value })}
             onKeyDown={this.handleKey}
-            />
-          </div>
+          />
+          <button className="btn search-btn" onClick={()=>this.props.searchMovies(this.state.search)}>SEARCH</button>
         </div>
-      
+        <div>
+          <label>
+            <input name="type" type="radio" />
+            <span>All</span>
+          </label>
+          <label>
+            <input name="type" type="radio" />
+            <span>Movies only</span>
+          </label>
+          <label>
+            <input name="type" type="radio" />
+            <span>Series only</span>
+          </label>
+        </div>
+      </div>
     );
   }
 }
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -5,30 +5,29 @@ import { Preloader } from "../components/Preloader";
 
 class Main extends React.Component {
   state = {
-    movies:[],
-  }
-  componentDidMount(){
-    fetch('http://www.omdbapi.com/?i=tt3896198&apikey=cf42352&s=matrix')
-    .then(response=>response.json())
-    .then(data=>this.setState({movies: data.Search}))
+    movies: [],
+  };
+  componentDidMount() {
+    fetch("http://www.omdbapi.com/?i=tt3896198&apikey=cf42352&s=matrix")
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search }));
   }
 
-searchMovies = (str) =>{
-  fetch('http://www.omdbapi.com/?i=tt3896198&apikey=cf42352&s=${str}')
-    .then(response=>response.json())
-    .then(data=>this.setState({movies: data.Search}))
-}
+  searchMovies = (str) => {
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=cf42352&s=${str}`)
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search }));
+  };
 
   render() {
-const{movies}=this.state;
+    const { movies } = this.state;
 
-    return <main className="container content">
-      <Search searchMovies = {this.searchMovies}/>
-      {
-        movies.length ? ( <Movies movies={this.state.movies}/>): <Preloader/>
-      }
-      
-    </main>;
+    return (
+      <main className="container content">
+        <Search searchMovies={this.searchMovies} />
+        {movies.length ? <Movies movies={this.state.movies} /> : <Preloader />}
+      </main>
+    );
   }
 }
 
