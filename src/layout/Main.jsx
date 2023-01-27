8525ac00e341bfb093825b310bb6bc008df86872
Kh82Olga <kh82olga@protonmail.com>
2023-01-27 12:34:41 +0100

    2701

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Movies } from "../components/Movies";
+import { Search } from "../components/Search";
+import { Preloader } from "../components/Preloader";
 
 class Main extends React.Component {
   state = {
@@ -10,12 +12,20 @@ class Main extends React.Component {
     .then(response=>response.json())
     .then(data=>this.setState({movies: data.Search}))
   }
+
+searchMovies = (str) =>{
+  fetch('http://www.omdbapi.com/?i=tt3896198&apikey=cf42352&s=${str}')
+    .then(response=>response.json())
+    .then(data=>this.setState({movies: data.Search}))
+}
+
   render() {
 const{movies}=this.state;
 
     return <main className="container content">
+      <Search searchMovies = {this.searchMovies}/>
       {
-        movies.length ? ( <Movies movies={this.state.movies}/>): <h5>Loading...</h5>
+        movies.length ? ( <Movies movies={this.state.movies}/>): <Preloader/>
       }
       
     </main>;
