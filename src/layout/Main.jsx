eac751b927b88d2fa67d85ceec50f8565c1c44e1
Kh82Olga <kh82olga@protonmail.com>
2023-02-01 14:28:50 +0100

    https

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -11,14 +11,14 @@ class Main extends React.Component {
     loading: true,
   };
   componentDidMount() {
-    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=matrix`)
+    fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=matrix`)
       .then((response) => response.json())
       .then((data) => this.setState({ movies: data.Search, loading: false }));
   }
 
   searchMovies = (str, type='all') => {
     this.setState({loading: true})
-    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${str}${type !=='all' ? `&type=${type}` : ''}`)
+    fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${str}${type !=='all' ? `&type=${type}` : ''}`)
       .then((response) => response.json())
       .then((data) => this.setState({ movies: data.Search, loading: false }));
   };
